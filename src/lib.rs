0b231d4bcdf737978dc47978c6363406506a7672
ladysamantha <ladysamantha@protonmail.com>
2020-07-12 19:26:07 -0600

    feat: use exec on unix

---
+++ src/lib.rs
@@ -0,0 +1,36 @@
+use std::{env, process::Command};
+
+#[cfg(unix)]
+pub fn run_cmd<I>(cmd: String, env_file: String, args: I) -> eyre::Result<()>
+where
+    I: Iterator<Item = <std::env::Args as Iterator>::Item>,
+{
+    use std::os::unix::process::CommandExt;
+    use eyre::Report;
+
+    set_env_from_file(env_file);
+
+    #[cfg(windows)]
+    let result = Command::new(cmd)
+        .args(args)
+        .spawn()
+        .map(|_| ())
+        .wrap_err("failed to spawn process");
+
+    // returning Err here since `exec` won't return on success
+    #[cfg(unix)]
+    let result = Err(Report::new(Command::new(cmd).args(args).exec()));
+
+    result
+}
+
+fn set_env_from_file(env_file: String) {
+    for line in env_file.lines() {
+        let split = line.split('=');
+        let split: Vec<&str> = split.collect();
+        if split.len() != 2 {
+            break;
+        }
+        env::set_var(split[0], split[1]);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,17 @@
-use std::{
-    collections::HashMap,
-    env,
-    fs,
-    path::Path,
-    process::Command,
-};
+use std::{env, fs, path::Path};
 
-use eyre::WrapErr;
+use xenv::run_cmd;
 
 fn main() -> eyre::Result<()> {
     let mut args = env::args().skip(1);
-    let cmd = args.next().ok_or_else(|| eyre::eyre!("missing first argument"))?;
-    let env_path = env::var("XENV_PATH")
-        .unwrap_or_else(|_| ".xenv".into());
+    let cmd = args.next().ok_or(eyre::eyre!("missing cmd"))?;
+    let env_path = env::var("XENV_PATH").unwrap_or(".xenv".into());
     let env_path = Path::new(&env_path);
     let env_file = if env_path.exists() {
         fs::read_to_string(env_path)?
     } else {
         "".into()
     };
-    let envs: HashMap<&str, &str> =
-            env_file
-            .lines()
-            .map(|line| {
-                let mut it = line.split('=');
-                (it.next().expect("missing first part"), it.next().expect("missing second part"))   
-            })
-            .filter(|(a, _)| a.len() > 0)
-            .collect();
-    Command::new(cmd)
-        .envs(envs)
-        .args(args)
-        .spawn()
-        .map(|_| ())
-        .wrap_err_with(|| eyre::eyre!("failed to spawn process"))
+
+    run_cmd(cmd, env_file, args)
 }
